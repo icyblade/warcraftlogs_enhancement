41cd50eb8ec426b1e52af0fa99e88035e3a7b9f1
Icyblade Dai <icyblade@users.noreply.github.com>
2017-07-14 10:22:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,129 +8,98 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-var attributes = ['critSpell', 'hasteSpell', 'mastery', 'versatilityDamageDone'];
+const attributes = ['critSpell', 'hasteSpell', 'mastery', 'versatilityDamageDone'];
+const columnNames = ['Crit', 'Haste', 'Mastery', 'Versatility'];
 
-for (var i = attributes.length - 1; i >= 0; i--) {
+const regex = /\/reports\/([\S\s]+?)#fight=([0-9]+)/;
 
-    $('<th class="sorting ui-state-default">' + attributes[i] + '</th>').insertBefore('th.zmdi.zmdi-flag.sorting.ui-state-default');
-}
-
-
+// initialize attribute columns
 for (var i = 0; i < attributes.length; i++) {
-    $('<td class="attr-' + attributes[i] + '"></td>').insertAfter('td.unique-gear');
+    $('<th class="sorting ui-state-default">' + columnNames[i] + '</th>').insertBefore('th.zmdi.zmdi-flag.sorting.ui-state-default');
+}
+for (var i = 0; i < attributes.length; i++) {
+    $('<td class="attr-' + attributes[i] + '"></td>').insertBefore('td.zmdi.zmdi-flag');
 }
 
-
-var regex = /\/reports\/([\S\s]+?)#fight=([0-9]+)/;
-
-
+// extract fights from ranking page
 var PlayerList = new Array();
 
 $('td.unique-gear').parent().each(function() {
-
     var player = new Object();
 
-    player.tid = $(this).attr('id');
+    player.rowID = $(this).attr('id');
     player.name = $(this).find('.players-table-name .main-table-player').text();
-    var href = $(this).find('.players-table-name .main-table-player').attr('href');
 
+    var href = $(this).find('.players-table-name .main-table-player').attr('href');
     player.logID = href.match(regex)[1];
     player.fightID = href.match(regex)[2];
 
     PlayerList.push(player);
-
 });
 
+
 function loadFights(index) {
-
-
     $.ajax({
-
         type: 'GET',
         url: 'https://www.warcraftlogs.com/reports/fights_and_participants/' + PlayerList[index].logID + '/0',
         dataType: 'json',
-
         success: function(data) {
-
-            console.info(index);
+            console.log(index);
             callback_fights(data, index);
         }
-
-
     });
-
 }
 
-
-function loadStats(tid, logID, fightID, timeStamp, sourceID) {
-
+function loadStats(rowID, logID, fightID, timestamp, sourceID) {
     $.ajax({
-
         type: 'GET',
-        url: 'https://www.warcraftlogs.com/reports/summary_events/' + logID + '/' + fightID + '/' + timeStamp + '/' + (timeStamp + 3000) + '/' + sourceID + '/0/Any/0/-1.0.-1/0',
+        url: 'https://www.warcraftlogs.com/reports/summary_events/' + logID + '/' + fightID + '/' + timestamp + '/' + (timestamp + 3000) + '/' + sourceID + '/0/Any/0/-1.0.-1/0',
         dataType: 'json',
-
         success: function(data) {
-
-            callback_stats(data, tid, logID, fightID, timeStamp, sourceID);
+            callback_stats(data, rowID, logID, fightID, timestamp, sourceID);
         }
-
-
     });
 }
 
-function callback_stats(data, tid, logID, fightID, timeStamp, sourceID) {
-
+function callback_stats(data, rowID, logID, fightID, timestamp, sourceID) {
     for (var key in attributes) {
         try {
-
-            $('#' + tid + ' .attr-' + attributes[key]).html(data.events[0][attributes[key]]);
+            $('#' + rowID + ' .attr-' + attributes[key]).html(data.events[0][attributes[key]]);
         } catch (e) {
-
-            console.info(e);
-            console.info(tid);
-            console.info(data);
+            console.error(e);
+            console.error(rowID);
+            console.error(data);
         }
     }
-
-
-
 }
 
-
 function callback_fights(data, idx) {
-
-
     PlayerList[idx].fight = data;
 
-    for (var j in PlayerList[idx].fight.friendlies) {
+    for (let j in PlayerList[idx].fight.friendlies) {
         if (PlayerList[idx].fight.friendlies[j].name == PlayerList[idx].name) {
             PlayerList[idx].sourceID = PlayerList[idx].fight.friendlies[j].id;
             break;
         }
     }
 
-    for (var j in PlayerList[idx].fight.fights) {
+    for (let j in PlayerList[idx].fight.fights) {
         if (PlayerList[idx].fight.fights[j].id == PlayerList[idx].fightID) {
-            PlayerList[idx].timeStamp = PlayerList[idx].fight.fights[j].start_time;
+            PlayerList[idx].timestamp = PlayerList[idx].fight.fights[j].start_time;
             break;
         }
     }
 
-    loadStats(PlayerList[idx].tid, PlayerList[idx].logID, PlayerList[idx].fightID, PlayerList[idx].timeStamp, PlayerList[idx].sourceID);
+    loadStats(PlayerList[idx].rowID, PlayerList[idx].logID, PlayerList[idx].fightID, PlayerList[idx].timestamp, PlayerList[idx].sourceID);
 
     idx++;
 
-
     if (idx >= PlayerList.length) {
-        console.info(PlayerList);
+        console.log(PlayerList);
         return;
     }
 
-
     loadFights(idx);
-
-
 }
 
 loadFights(0);
