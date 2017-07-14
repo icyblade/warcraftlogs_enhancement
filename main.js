fec4157068c5c597a732d313e2577f78ba2bd621
Icyblade Dai <icyblade@users.noreply.github.com>
2017-07-14 16:06:10 +0800

    update version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Warcraft Logs Enhancement
 // @namespace    https://github.com/icyblade/warcraftlogs_enhancement
-// @version      0.1
+// @version      0.2
 // @description  Some Enhancement Scripts of Warcraft Logs
 // @author       swqsldz, kingofpowers, icyblade
 // @match        https://*.warcraftlogs.com/*
